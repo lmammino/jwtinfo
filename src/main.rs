8802179ea997eea770a6837f0adc2cba7adddb73
Luciano Mammino <lmammino@vectra.ai>
2019-12-11 10:33:52 +0000

    decoding header from JSON

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,25 @@ use std::env;
 use std::process;
 use std::str;
 use base64;
+use serde::Deserialize;
 
-struct Token {
-    header: String,
-    body: String,
+#[derive(Deserialize, Debug)]
+struct JWTHeader {
+    typ: String,
+    alg: String
+}
+
+#[derive(Debug)]
+struct JWTToken<'a> {
+    header: &'a JWTHeader,
+    body: &'a str,
     signature: Vec<u8>
+}
+
+impl<'a> JWTToken<'a> {
+    pub fn new(header: &'a JWTHeader, body: &'a str, signature: Vec<u8>) -> Self {
+        JWTToken { header, body, signature }
+    }
 }
 
 fn parse_base64_string (s: &str) -> Result<&str, &str> {
@@ -19,14 +33,18 @@ fn parse_base64_string (s: &str) -> Result<&str, &str> {
     }
 }
 
-fn parse_jwt_token (token: &str) -> Result<&Token, &str> {
+fn parse_jwt_token (token: &str) -> Result<&JWTToken, &str> {
     let parts : Vec<&str> = token.split('.').collect();
-    let header = match parts.get(0) {
+    let header_str = match parts.get(0) {
         Some(s) => match parse_base64_string(s) {
             Ok(s) => s,
             Err(e) => return Err(&format!("Malformed JWT: Header {}", e))
         },
         None => return Err("Malformed JWT: Header is missing")
+    };
+    let header: JWTHeader = match serde_json::from_str(header_str) {
+        Ok(h) => h,
+        Err(e) => return Err("Malformed JWT: Header is not a valid JSON")
     };
 
     let body = match parts.get(1) {
@@ -38,7 +56,8 @@ fn parse_jwt_token (token: &str) -> Result<&Token, &str> {
     };
 
     // TODO
-    // parse body
+    // x JSON parse header
+    // x parse body
     // parse signature
     // return new Token instance
 }
