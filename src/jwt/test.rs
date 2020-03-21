ce41401c29b84077a807e374a41f27b054f85f1a
sabalsamo <sabalsamo@vectra.ai>
2020-03-21 12:24:51 +0000

    enhancement/track-code-coverage-and-increase-tests

--- src/jwt/test.rs
+++ src/jwt/test.rs
@@ -6,3 +6,54 @@ fn assert_parse_token_successfully() {
     let jwt_token = parse_token(&token);
     assert_eq!(String::from("{\"foo\":\"bar\"}"), jwt_token.unwrap().body);
 }
+
+#[test]
+fn assert_parse_token_fails_due_to_invalid_header() {
+    let token = String::from(
+        "invalid_header.eyJmb28iOiJiYXIifQ.dtxWM6MIcgoeMgH87tGvsNDY6cHWL6MGW4LeYvnm1JA",
+    );
+    let jwt_token = parse_token(&token);
+    assert!(
+        true,
+        jwt_token
+            .unwrap_err()
+            .to_string()
+            .contains("Invalid Header:")
+    );
+}
+
+#[test]
+fn assert_parse_token_fails_due_to_invalid_body() {
+    let token = String::from(
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.invalid_body.dtxWM6MIcgoeMgH87tGvsNDY6cHWL6MGW4LeYvnm1JA",
+    );
+    let jwt_token = parse_token(&token);
+    assert!(
+        true,
+        jwt_token.unwrap_err().to_string().contains("Invalid Body:")
+    );
+}
+
+#[test]
+fn assert_parse_token_fails_due_to_invalid_signature() {
+    let token =
+        String::from("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIifQ.invalid_signature");
+    let jwt_token = parse_token(&token);
+    assert!(
+        true,
+        jwt_token
+            .unwrap_err()
+            .to_string()
+            .contains("Invalid Signature:")
+    );
+}
+
+#[test]
+fn assert_parse_token_fails_due_to_more_then_three_fragment() {
+    let token = String::from("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIifQ.dtxWM6MIcgoeMgH87tGvsNDY6cHWL6MGW4LeYvnm1JA.one_more_fragment");
+    let jwt_token = parse_token(&token);
+    assert_eq!(
+        String::from("Error: Unexpected fragment after signature"),
+        jwt_token.unwrap_err().to_string()
+    );
+}
