72b2822befe5d4144cb8e27a68822e974f9b6e88
cicciodb@hotmail.it <cicciodb@hotmail.it>
2026-08-06 22:23:36 +0200

    update deprecated Command::cargo_bin

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -1,4 +1,4 @@
-use assert_cmd::Command;
+use assert_cmd::cargo::*;
 use predicates::prelude::*;
 
 const TEST_JWT: &str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIifQ.dtxWM6MIcgoeMgH87tGvsNDY6cHWL6MGW4LeYvnm1JA";
@@ -7,7 +7,7 @@ const TEST_JWE_DECRYPTED: &str = "This is a super secret message!";
 
 #[test]
 fn test_default_shows_body() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg(TEST_JWT)
         .assert()
         .success()
@@ -16,7 +16,7 @@ fn test_default_shows_body() {
 
 #[test]
 fn test_header_flag_shows_header() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("--header")
         .arg(TEST_JWT)
         .assert()
@@ -26,7 +26,7 @@ fn test_header_flag_shows_header() {
 
 #[test]
 fn test_full_flag_shows_both_header_and_claims() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("--full")
         .arg(TEST_JWT)
         .assert()
@@ -39,7 +39,7 @@ fn test_full_flag_shows_both_header_and_claims() {
 
 #[test]
 fn test_full_flag_with_pretty() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("--full")
         .arg("--pretty")
         .arg(TEST_JWT)
@@ -53,7 +53,7 @@ fn test_full_flag_with_pretty() {
 
 #[test]
 fn test_full_and_header_flags_conflict() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("--full")
         .arg("--header")
         .arg(TEST_JWT)
@@ -64,7 +64,7 @@ fn test_full_and_header_flags_conflict() {
 
 #[test]
 fn test_pretty_flag_formats_output() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("--pretty")
         .arg(TEST_JWT)
         .assert()
@@ -74,7 +74,7 @@ fn test_pretty_flag_formats_output() {
 
 #[test]
 fn test_stdin_input() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("-")
         .write_stdin(TEST_JWT)
         .assert()
@@ -84,7 +84,7 @@ fn test_stdin_input() {
 
 #[test]
 fn test_invalid_jwt_returns_error() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("invalid.jwt.token")
         .assert()
         .success()
@@ -93,7 +93,7 @@ fn test_invalid_jwt_returns_error() {
 
 #[test]
 fn test_full_flag_structure() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     let output = cmd.arg("--full").arg(TEST_JWT).output().unwrap();
 
     let stdout = String::from_utf8(output.stdout).unwrap();
@@ -115,7 +115,7 @@ fn test_full_flag_structure() {
 // JWE (encrypted JWT) tests
 #[test]
 fn test_jwe_shows_encrypted_message() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg(TEST_JWE)
         .assert()
         .success()
@@ -126,7 +126,7 @@ fn test_jwe_shows_encrypted_message() {
 
 #[test]
 fn test_jwe_header_flag_shows_header() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("--header")
         .arg(TEST_JWE)
         .assert()
@@ -137,7 +137,7 @@ fn test_jwe_header_flag_shows_header() {
 
 #[test]
 fn test_jwe_full_flag_shows_header_and_encrypted_message() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("--full")
         .arg(TEST_JWE)
         .assert()
@@ -152,7 +152,7 @@ fn test_jwe_full_flag_shows_header_and_encrypted_message() {
 
 #[test]
 fn test_jwe_pretty_flag() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     cmd.arg("--pretty")
         .arg(TEST_JWE)
         .assert()
@@ -164,7 +164,7 @@ fn test_jwe_pretty_flag() {
 
 #[test]
 fn test_jwe_with_key_decrypts_payload() {
-    let mut cmd = Command::cargo_bin("jwtinfo").unwrap();
+    let mut cmd = cargo_bin_cmd!("jwtinfo");
     let key_path = format!(
         "{}/src/jwe/tests/fixtures/priv_simple_token.pem",
         env!("CARGO_MANIFEST_DIR")
